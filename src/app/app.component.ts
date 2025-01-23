726b127419487994aa79b322308d6a82b28336a6
matheusdalbone <matheusdalbone7@gmail.com>
2025-01-23 10:41:37 -0300

    feat: header, navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BackgroundComponent } from './components/background/background.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { loadStarsPreset } from '@tsparticles/preset-stars';
+import { NgxParticlesModule } from '@tsparticles/angular';
+import { Engine } from '@tsparticles/engine';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [BackgroundComponent]
+  imports: [NgxParticlesModule, NavbarComponent]
 })
 export class AppComponent {
   title = 'portfolio';
+
+    id = "tsparticles";
+  
+    particleOptions = {
+      preset: "stars",
+    };
+  
+    particlesInit = async (engine: Engine): Promise<void> => {
+      
+      await loadStarsPreset(engine);
+    };
+  
 }
